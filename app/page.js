78662d75c962288b1797f38a8f2e95fbe6f41634
royhoo <cnroyhoo@gmail.com>
2023-03-25 13:54:37 +0800

    change function name

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ import { providers, Contract } from "ethers";
 import { useEffect, useRef, useState } from "react";
 import { WHITELIST_CONTRACT_ADDRESS, abi } from "../constants";
 
-export default function page() {
+export default function Page() {
   // walletConnected keep track of whether the user's wallet is connected or not
   const [walletConnected, setWalletConnected] = useState(false);
   // joinedWhitelist keeps track of whether the current metamask address has joined the Whitelist or not
